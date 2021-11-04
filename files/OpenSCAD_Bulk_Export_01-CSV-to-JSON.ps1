11df405748e2000745d8d61c91f54edc42ee0a1c
OutwardBuckle <50000826+OutwardBuckle@users.noreply.github.com>
2021-11-04 14:49:17 +0000

    Update OpenSCAD_Bulk_Export_01-CSV-to-JSON.ps1

--- files/OpenSCAD_Bulk_Export_01-CSV-to-JSON.ps1
+++ files/OpenSCAD_Bulk_Export_01-CSV-to-JSON.ps1
@@ -27,7 +27,6 @@ $scadPath = $FileBrowser.FileName
 ##################
 
 $JsonPath = (Get-ItemProperty $scadPath).DirectoryName + '\' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
-$backupJsonPath = (Get-ItemProperty $scadPath).DirectoryName + '\' + (Get-Date -Format 'yyMMdd-hhmm') + '-' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
 
 if(Test-Path -Path $JsonPath){
     cls
