d1a0ef6b9de61f6cc64f507e04fea6eb74f70592
OutwardBuckle <50000826+OutwardBuckle@users.noreply.github.com>
2022-01-16 17:40:27 +0000

    Update 04-SCAD-to-CSV.ps1
    Now working on Linux

--- files/04-SCAD-to-CSV.ps1
+++ files/04-SCAD-to-CSV.ps1
@@ -1,62 +1,98 @@
-﻿### You will be prompted for these values if not set here
+### You will be prompted for these values if not set here
 $scadPath = ""
-$fileExe = "" # Requires latest dev build as of 2021-11
 
 ################ START OF SCRIPT
 
-Add-Type -AssemblyName System.Windows.Forms
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
-$FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
 function pause{ $null = Read-Host 'Press Any Key or Enter to continue...' }
+if($IsWindows){
 
-##################
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
 
-If ($scadPath -eq ''){
+    ##################
 
-    cls
-    echo "You will now be prompted for the SCAD file"
-    pause
+    If ($scadPath -eq ''){
 
-    $FileBrowser.filter = "scad (*.scad)| *.scad"
-    [void]$FileBrowser.ShowDialog()
+        Clear-Host
+        Write-Output "You will now be prompted for the SCAD file"
+        pause
 
-    $scadPath = $FileBrowser.FileName
+        $FileBrowser.filter = "scad (*.scad)| *.scad"
+        [void]$FileBrowser.ShowDialog()
+
+        $scadPath = $FileBrowser.FileName
+    }
+
+    ##################
+
+    $fileExe = "C:\Program Files\OpenSCAD Dev\openscad.exe" # Requires latest dev build as of 2021-11
+
+    If ($fileExe -eq ''){
+
+        Clear-Host
+        Write-Output "Select the OpenSCAD .exe file (requires latest dev build as of 2021-11)"
+        pause
+
+        $FileBrowser.filter = "exe (*.exe)| *.exe"
+        [void]$FileBrowser.ShowDialog()
+
+        $fileExe = $FileBrowser.FileName
+
+    }
+
+    ##################
+
+    $CSVfile = (Get-ItemProperty $scadPath).DirectoryName + '\' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".csv"
+
+} elseif($IsLinux) {
+
+    If ($scadPath -eq ''){
+        Clear-Host
+        $scadPath = Read-Host -Prompt 'Input the full .SCAD file path'
+    }
+
+    ##################
+
+    $CSVfile = (Get-ItemProperty $scadPath).DirectoryName + '/' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
+
+} else {
+    Pause
+    Write-Host "Max OS not yet supported"    
 }
 
 ##################
 
-If ($fileExe -eq ''){
 
-    cls
-    echo "Select the OpenSCAD .exe file (requires latest dev build as of 2021-11)"
-    pause
-
-    $FileBrowser.filter = "exe (*.exe)| *.exe"
-    [void]$FileBrowser.ShowDialog()
-
-    $fileExe = $FileBrowser.FileName
-
+if(Test-Path -Path $CSVfile){
+    $CSVfile = (Get-ItemProperty $scadPath).DirectoryName + '\' + (Get-Date -Format 'yyMMdd-hhmm') + '-' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".csv"
 }
 
-##################
 
-# Output CSV File
-$CSVfile = (Get-ItemProperty $scadPath).DirectoryName + '\' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".csv"
 # Path for temporary json output file
 $tempJsonPath = (Get-ItemProperty $scadPath).DirectoryName + '\' + (Get-Date -Format 'yyMMdd-hhmm') + '-temp-' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
 # Set the arguments
 $arguments = '--export-format param -o "' + $tempJsonPath + '" "' + $scadPath + '"'
 # Run the command
-start-process $fileExe $arguments -Wait
+if($IsWindows){
+    start-process $using:fileExe $arguments -Wait
+} else {
+    start-process openscad $arguments -Wait
+}
 # Build table with name
 $myObject = [PSCustomObject]@{
-    exported_filename     = 'Example'
+    exported_filename     = 'default'
 }
 # Add each item to the table
-(Get-Content $tempJsonPath | ConvertFrom-Json).parameters | foreach {
+(Get-Content $tempJsonPath | ConvertFrom-Json).parameters | ForEach-Object {
     $myObject | Add-Member -MemberType NoteProperty -Name $_.name -Value $_.initial
 }
 # Export to CSV
 $myObject | export-csv -Path $CSVfile -NoTypeInformation
 # Cleanup JSON file
 Remove-Item -Path $tempJsonPath
+
+Clear-Host
+write-host "CSV file has been created:
+$CSVfile"
+
+pause
