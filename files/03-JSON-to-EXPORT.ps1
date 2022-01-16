db86e1057ce28187c48cdca0b92d4a4729026981
OutwardBuckle <50000826+OutwardBuckle@users.noreply.github.com>
2022-01-16 17:40:09 +0000

    Update 03-JSON-to-EXPORT.ps1
    Now working on Linux and includes will run multiple exports in parrall if using PowerShell 7

--- files/03-JSON-to-EXPORT.ps1
+++ files/03-JSON-to-EXPORT.ps1
@@ -3,185 +3,291 @@ $scadPath = "" ### Path to the .SCAD file
 $OutputFolder = "" ### Path of output folder
 $file_extension = "" ### "STL", "OFF", "AMF", "3MF", "DXF", "SVG", "PNG"
 $cam_args = "" ### Only needed if using PNG export
+$process_count = 3
 
 ################ START OF SCRIPT
 
-Add-Type -AssemblyName System.Windows.Forms
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
-$FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
-function pause{ $null = Read-Host 'Press Any Key or Enter to continue...' }
+function pause{ $null = Read-Host 'Press Enter to continue...' }
 
-##################
+if($IsWindows){
 
-If ($scadPath -eq ''){
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+    $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
 
-    cls
-    echo "You will now be prompted for the SCAD file"
-    pause
+    ##################
 
-    $FileBrowser.filter = "scad (*.scad)| *.scad"
-    [void]$FileBrowser.ShowDialog()
+    If ($scadPath -eq ''){
 
-    $scadPath = $FileBrowser.FileName
-}
+        Clear-Host
+        Write-Output "You will now be prompted for the SCAD file"
+        pause
 
-##################
+        $FileBrowser.filter = "scad (*.scad)| *.scad"
+        [void]$FileBrowser.ShowDialog()
 
-If ($OutputFolder -eq ''){
-
-    cls
-    echo "You will now be prompted for the STL OUTPUT folder"
-    pause
-
-    [void]$FolderBrowser.ShowDialog()
-
-    $OutputFolder = $FolderBrowser.SelectedPath
-}
-
-If(!(test-path $OutputFolder))
-{
-      New-Item -ItemType Directory -Force -Path $OutputFolder
-}
-
-##################
-
-If ($file_extension -eq ''){
-
-    $file_extension_list = @(
-        "STL", "OFF", "AMF", "3MF", "DXF", "SVG", "PNG"
-    )
-
-    $GridArguments = @{
-        OutputMode = 'Single'
-        Title      = 'Please select a export format and click OK'
+        $scadPath = $FileBrowser.FileName
     }
 
-    $file_extension = $file_extension_list | Sort-Object | Out-GridView @GridArguments | foreach {
-        $_
+    ##################
+
+    If ($OutputFolder -eq ''){
+
+        Clear-Host
+        Write-Output "You will now be prompted for the STL OUTPUT folder"
+        pause
+
+        [void]$FolderBrowser.ShowDialog()
+
+        $OutputFolder = $FolderBrowser.SelectedPath
+    } elseif ($OutputFolder -notmatch '\\$'){
+        $OutputFolder += '\'
     }
 
+    If(!(test-path $OutputFolder))
+    {
+        New-Item -ItemType Directory -Force -Path $OutputFolder
+    }
 
+    ##################
 
-    If ($file_extension -eq "PNG" -and $cam_args -eq ''){
+    If ($file_extension -eq ''){
 
-    cls
-    Write-Host "Default Camera Settings
-    Leave Blank
+        $file_extension_list = @(
+            "STL", "OFF", "AMF", "3MF", "DXF", "SVG", "PNG"
+        )
 
-Specific Camera Location ( translate_x,y,z, rot_x,y,z, dist)
-    --camera 0,0,0,120,0,50,140
+        $GridArguments = @{
+            OutputMode = 'Single'
+            Title      = 'Please select a export format and click OK'
+        }
 
-Auto-center
-    --autocenter
+        $file_extension = $file_extension_list | Sort-Object | Out-GridView @GridArguments | ForEach-Object {
+            $_
+        }
 
-View All
-    --viewall
- 
-Color Scheme
-    --colorscheme DeepOcean
+    }
 
-User Render Quality
-    --render
+    ##################
 
-Set Image Export Size
-    --imgsize 100,100
+    if(Test-Path -Path "C:\Program Files\OpenSCAD\openscad.exe"){
+        $fileExe = "C:\Program Files\OpenSCAD\openscad.exe"
+    } else {
+        Clear-Host
+        Write-Output "OpenSCAD .exe file not found in default location, you will need to select the .exe file manually"
+        pause
 
+        $FileBrowser.filter = "exe (*.exe)| *.exe"
+        [void]$FileBrowser.ShowDialog()
 
+        $fileExe = $FileBrowser.FileName
+    }
 
-Example - Top Down, View All, Auto-Center, Deep Ocean Color Scheme
-    --camera 0,0,0,0,0,0,0 --viewall --autocenter --colorscheme DeepOcean
+    ##################
 
-Example - Default camera position, View All, Auto-Center, Deep Ocean Color Scheme
-    --viewall --autocenter --imgsize 1024,1024 --render --colorscheme DeepOcean
+    $JsonPath = (Get-ItemProperty $scadPath).DirectoryName + '\' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
+
+} elseif($IsLinux) {
+
+        If ($scadPath -eq ''){
+            Clear-Host
+            $scadPath = Read-Host -Prompt 'Input the full .SCAD file path'
+        }
+        
+        ##################
+        
+        If ($OutputFolder -eq ''){
+        
+            Clear-Host
+            $OutputFolder = Read-Host -Prompt 'Input the export folder path'
+
+            if ($OutputFolder -notmatch '\\$'){
+                $OutputFolder += '\'
+            }
+        
+            If(!(test-path $OutputFolder))
+            {
+                New-Item -ItemType Directory -Force -Path $OutputFolder
+            }
     
-"
+        }
 
-    $cam_args = Read-Host -Prompt 'Input the camera arguments then press enter'
+        ################
+        
+        If ($file_extension -eq ''){
+        
+            Write-Host "Enter one of the following options: STL, OFF, AMF, 3MF, DXF, SVG, PNG"
+        
+            $file_extension = Read-Host -Prompt 'Input the file extension'
+    
+        }
 
-    }
+        ################
 
-}
-
-##################
-
-if(Test-Path -Path "C:\Program Files\OpenSCAD\openscad.exe"){
-    $fileExe = "C:\Program Files\OpenSCAD\openscad.exe"
+        $JsonPath = (Get-ItemProperty $scadPath).DirectoryName + '/' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
+        $fileExe = ' '
+        
 } else {
-    cls
-    echo "OpenSCAD .exe file not found in default location, you will need to select the .exe file manually"
-    pause
-
-    $FileBrowser.filter = "exe (*.exe)| *.exe"
-    [void]$FileBrowser.ShowDialog()
-
-    $fileExe = $FileBrowser.FileName
+    Pause
+    Write-Host "Max OS not yet supported"    
 }
-
-##################
 
 $JsonPath = (Get-ItemProperty $scadPath).DirectoryName + '\' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
 
-##### START STL EXPORT #####
-# Import JSON
-If(Test-Path -Path $JsonPath){
+####### FILE EXPORT
+
+if((Get-Host | Select-Object Version).Version.Major -gt 6){
 
     $importedJSON = Get-Content $jsonPath | convertfrom-json
-
-    # Count Items
     $totalItemCount = (Get-Member -MemberType NoteProperty -InputObject $importedJSON[0].parameterSets).count
+    $dataset = @()
 
-    # For Each Item
     $importedJSON[0].parameterSets | Get-Member -MemberType NoteProperty | ForEach-Object {
-    
-        # Increase the count by 1
-        $current_count += 1
-
+        $index += 1
         # Set the current file name to Parameter Set Name + .stl
         $Output_Filename = $_.Name + '.' + $file_extension
         # Set the Output Path
         if ($OutputFolder -match '\\$'){
             $OutputPath = $OutputFolder + $Output_Filename
         } else {
-             $OutputPath = $OutputFolder + '\' + $Output_Filename
-        }
-
-        ## Write Host
-        #write-host ("Export Path " + $OutputPath)
-
-        Write-Progress -Activity ("Exporting " + $Output_Filename) -Status ("Progress: " + $current_count + " of " + $totalItemCount) -PercentComplete ($current_count/$totalItemCount*100)
-
-        ## Check if item exists, if not
-        if (-not(Test-Path -Path $OutputPath)) {
-
-            # Set the arguments
-            If ($file_extension -eq "PNG"){
-                $arguments = '-o "' + $OutputPath + '" ' + $cam_args + ' -p "' + $jsonPath + '" -P "' + $_.Name + '" "' + $scadPath + '"'
+            if($IsWindows){
+                $OutputPath = $OutputFolder + '\' + $Output_Filename
             } else {
-                $arguments = '-o "' + $OutputPath + '" -p "' + $jsonPath + '" -P "' + $_.Name + '" "' + $scadPath + '"'
+                $OutputPath = $OutputFolder + $Output_Filename
             }
-
-            # Run the command
-            start-process $fileExe $arguments -Wait -RedirectStandardError NUL
-
-            $export_count += 1
-
-        } else {
-             $skip_count += 1
         }
 
+        # Set the arguments
+        If ($file_extension -eq "PNG"){
+            $arguments = '-o "' + $OutputPath + '" ' + $cam_args + ' -p "' + $jsonPath + '" -P "' + $_.Name + '" "' + $scadPath + '"'
+        } else {
+            $arguments = '-o "' + $OutputPath + '" -p "' + $jsonPath + '" -P "' + $_.Name + '" "' + $scadPath + '"'
+        }
+
+        # Run the command
+        $current_data_set = @{
+            Id=$index
+            Value=$arguments
+            Name=$_.Name
+        }
+        $dataset += $current_data_set
     }
 
-    cls
-    Write-Host "Exported: $export_count
-Skipped: $skip_count
-Total: $totalItemCount"
+    # Create a hashtable for process.
+    # Keys should be ID's of the processes
+    $origin = @{}
+    $dataset | Foreach-Object {$origin.($_.id) = @{}}
 
+    # Create synced hashtable
+    $sync = [System.Collections.Hashtable]::Synchronized($origin)
+
+    $job = $dataset | Foreach-Object -ThrottleLimit $process_count -AsJob -Parallel {
+        $syncCopy = $using:sync
+        $process = $syncCopy.$($PSItem.Id)
+
+        $process.Id = $PSItem.Id
+        $process.Activity = "$($PSItem.Name).$using:file_Extension - $($PSItem.Id) of $using:totalItemCount"
+        $process.Status = "Processing"
+
+        #$PSItem.Value
+        $arguments = $PSItem.Value
+        if($IsWindows){
+            start-process $using:fileExe $arguments -Wait -RedirectStandardError NUL
+        } else {
+            start-process openscad $arguments -Wait -RedirectStandardError NUL
+        }
+
+        # Mark process as completed
+        $process.Completed = $true
+    }
+
+    # Show progress
+    while($job.State -eq 'Running')
+    {
+        $sync.Keys | Foreach-Object {
+            # If key is not defined, ignore
+            if(![string]::IsNullOrEmpty($sync.$_.keys))
+            {
+                # Create parameter hashtable to splat
+                $param = $sync.$_
+
+                # Execute Write-Progress
+                Write-Progress @param
+            }
+        }
+
+        # Wait to refresh to not overload gui
+        Start-Sleep -Seconds 0.1
+    }
+
+    # Write-Host "Done..."
     pause
 
 } else {
-    
-    cls
-    Write-host "Missing JSON file..."
-    pause
+
+    ##### START STL EXPORT #####
+    # Import JSON
+    If(Test-Path -Path $JsonPath){
+
+        $importedJSON = Get-Content $jsonPath | convertfrom-json
+
+        # Count Items
+        $totalItemCount = (Get-Member -MemberType NoteProperty -InputObject $importedJSON[0].parameterSets).count
+
+        # For Each Item
+        $importedJSON[0].parameterSets | Get-Member -MemberType NoteProperty | ForEach-Object {
+        
+            # Increase the count by 1
+            $current_count += 1
+
+            # Set the current file name to Parameter Set Name + .stl
+            $Output_Filename = $_.Name + '.' + $file_extension
+            # Set the Output Path
+            if ($OutputFolder -match '\\$'){
+                $OutputPath = $OutputFolder + $Output_Filename
+            } else {
+                $OutputPath = $OutputFolder + '\' + $Output_Filename
+            }
+
+            ## Write Host
+            #write-host ("Export Path " + $OutputPath)
+
+            Write-Progress -Activity ("Exporting " + $Output_Filename) -Status ("Progress: " + $current_count + " of " + $totalItemCount) -PercentComplete ($current_count/$totalItemCount*100)
+
+            ## Check if item exists, if not
+            if (-not(Test-Path -Path $OutputPath)) {
+
+                # Set the arguments
+                If ($file_extension -eq "PNG"){
+                    $arguments = '-o "' + $OutputPath + '" ' + $cam_args + ' -p "' + $jsonPath + '" -P "' + $_.Name + '" "' + $scadPath + '"'
+                } else {
+                    $arguments = '-o "' + $OutputPath + '" -p "' + $jsonPath + '" -P "' + $_.Name + '" "' + $scadPath + '"'
+                }
+
+                # Run the command
+                start-process $fileExe $arguments -Wait -RedirectStandardError NUL
+
+                $export_count += 1
+
+            } else {
+                $skip_count += 1
+            }
+
+        }
+
+        Clear-Host
+        Write-Host "Exported: $export_count
+    Skipped: $skip_count
+    Total: $totalItemCount"
+
+        pause
+
+    } else {
+        
+        Clear-Host
+        Write-host "Missing JSON file..."
+        pause
+
+    }
 
 }
