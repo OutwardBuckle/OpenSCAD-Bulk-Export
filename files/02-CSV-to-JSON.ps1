8e7538d23d2de440f7cd8a9bab4d054fccd10505
OutwardBuckle <50000826+OutwardBuckle@users.noreply.github.com>
2022-01-16 17:39:51 +0000

    Update 02-CSV-to-JSON.ps1
    Now working on Linux

--- files/02-CSV-to-JSON.ps1
+++ files/02-CSV-to-JSON.ps1
@@ -4,53 +4,82 @@ $scadPath = ""
 
 ################ START OF SCRIPT
 
-Add-Type -AssemblyName System.Windows.Forms
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
-function pause{ $null = Read-Host 'Press Any Key or Enter to continue...' }
+function pause{ $null = Read-Host 'Press Enter to continue...' }
 
-################
+if($IsWindows){
 
-If ($csvPath -eq ''){
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
 
-    cls
-    echo "You will now be prompted for the CSV file"
-    pause
+    ################
 
-    $FileBrowser.filter = "csv (*.csv)| *.csv"
-    [void]$FileBrowser.ShowDialog()
+    If ($csvPath -eq ''){
 
-    $csvPath = $FileBrowser.FileName
+        Clear-Host
+        Write-Output "You will now be prompted for the CSV file"
+        pause
 
+        $FileBrowser.filter = "csv (*.csv)| *.csv"
+        [void]$FileBrowser.ShowDialog()
+
+        $csvPath = $FileBrowser.FileName
+
+    }
+
+    ##################
+
+    If ($scadPath -eq ''){
+
+        Clear-Host
+        Write-Output "You will now be prompted for the SCAD file"
+        pause
+
+        $FileBrowser.filter = "scad (*.scad)| *.scad"
+        [void]$FileBrowser.ShowDialog()
+
+        $scadPath = $FileBrowser.FileName
+    }
+
+    ##################
+
+    $JsonPath = (Get-ItemProperty $scadPath).DirectoryName + '\' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
+
+} elseif($IsLinux) {
+
+    If ($null -eq $csvPath){
+        Clear-Host
+        $csvPath = Read-Host -Prompt 'Input the full CSV path'
+    
+    }
+    
+    ##################
+    
+    If ($scadPath -eq ''){
+        Clear-Host
+        $scadPath = Read-Host -Prompt 'Input the full .SCAD file path'
+    }
+
+    ##################
+
+    $JsonPath = (Get-ItemProperty $scadPath).DirectoryName + '/' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
+
+} else {
+    Pause
+    Write-Host "Max OS not yet supported"    
 }
 
 ##################
-
-If ($scadPath -eq ''){
-
-    cls
-    echo "You will now be prompted for the SCAD file"
-    pause
-
-    $FileBrowser.filter = "scad (*.scad)| *.scad"
-    [void]$FileBrowser.ShowDialog()
-
-    $scadPath = $FileBrowser.FileName
-}
-
-##################
-
-$JsonPath = (Get-ItemProperty $scadPath).DirectoryName + '\' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json"
 
 if(Test-Path -Path $JsonPath){
-    cls
-    echo ".JSON file already exists, creating backup..."
+    Clear-Host
+    Write-Output ".JSON file already exists, creating backup..."
     Copy-Item -Path $JsonPath -Destination ((Get-ItemProperty $scadPath).DirectoryName + '\' + (Get-Date -Format 'yyMMdd-hhmm') + '-' + [System.IO.Path]::GetFileNameWithoutExtension($scadPath) + ".json")
     Write-Host "----"
     Write-Host ""
 } 
 
 ##### START JSON EXPORT #####
-echo "Creating JSON File..."
+Write-Output "Creating JSON File..."
 # Build the json structure
 $jsonBase = @{}
 $parameterSets = @{}
@@ -59,7 +88,7 @@ $parameterSets = @{}
 $csvData = Import-Csv -Path $csvPath
 
 # For each item:
-$csvData | foreach {
+$csvData | ForEach-Object {
     
     # Set the name for the current parameter set
     $parameterSetName = $_.exported_filename
@@ -74,7 +103,7 @@ $jsonBase.Add("parameterSets",$parameterSets)
 # Output to a file
 $jsonBase | ConvertTo-Json | Set-Content $jsonPath
 
-cls
+Clear-Host
 write-host "JSON file has been created:
 $jsonPath"
 pause
